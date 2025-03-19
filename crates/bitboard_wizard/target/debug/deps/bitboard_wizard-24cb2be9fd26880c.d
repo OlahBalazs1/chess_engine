6681036f22ab952c9d8096ec17e0a49ca8d6d186
OlahBalazs1 <olah.balazs@students.jedlik.eu>
2025-03-19 20:51:12 +0100

    set workspace

---
+++ crates/bitboard_wizard/target/debug/deps/bitboard_wizard-24cb2be9fd26880c.d
@@ -0,0 +1,5 @@
+/DDrive/Other/vscode/chess_engine/crates/bitboard_wizard/target/debug/deps/libbitboard_wizard-24cb2be9fd26880c.rmeta: src/lib.rs
+
+/DDrive/Other/vscode/chess_engine/crates/bitboard_wizard/target/debug/deps/bitboard_wizard-24cb2be9fd26880c.d: src/lib.rs
+
+src/lib.rs:
